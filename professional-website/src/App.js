d32fa6d98c1de728ce6058031c5b52b23bc42585
CRIS66666 <eliao014@ucr.edu>
2024-05-16 00:03:56 -0700

    minor changes

--- professional-website/src/App.js
+++ professional-website/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import NavigationBar from './NavigationBar';
 import HeroSection from './HeroSection';
 import AboutSection from './AboutSection';
